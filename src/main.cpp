dbb8c4ebae4e238706057aae7f3ba7ec556d3616
Fuzzbawls <fuzzbawls@gmail.com>
2019-02-23 02:09:05 -0800

    Fix segfault with runtime -disablewallet
    Signed-off-by: observerdev <dev@obsr.org>

--- src/main.cpp
+++ src/main.cpp
@@ -3331,9 +3331,11 @@ void static UpdateTip(CBlockIndex* pindexNew)
 {
     chainActive.SetTip(pindexNew);
 
+#ifdef ENABLE_WALLET
     // If turned on AutoZeromint will automatically convert OBSR to zOBSR
-    if (pwalletMain->isZeromintEnabled ())
-        pwalletMain->AutoZeromint ();
+    if (pwalletMain && pwalletMain->isZeromintEnabled())
+        pwalletMain->AutoZeromint();
+#endif // ENABLE_WALLET
 
     // New best block
     nTimeBestReceived = GetTime();
